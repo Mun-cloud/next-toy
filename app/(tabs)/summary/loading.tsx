7e13f99ad0cbd41df9bddcb6857f07135a0f95cb
Taeho <mun05170@gmail.com>
2024-07-04 18:23:50 +0900

    Feat: 스켈레톤

--- app/(tabs)/summary/loading.tsx
+++ app/(tabs)/summary/loading.tsx
@@ -1,5 +1,36 @@
 const PostsLoading = () => {
-  return <div>PostsLoading</div>;
+  return (
+    <div>
+      <div className="border-b py-3 px-5 h-[67px] flex justify-between items-center">
+        <div className="flex flex-col gap-1 grow h-full">
+          <div className="bg-gray-400 animate-pulse rounded-md w-[50%] h-full" />
+          <div className="bg-gray-400 animate-pulse rounded-md w-[20%] h-full" />
+        </div>
+        <div className="bg-gray-400 animate-pulse rounded-full size-[30px]" />
+      </div>
+      <div className="border-b py-3 px-5 h-[67px] flex justify-between items-center">
+        <div className="flex flex-col gap-1 grow h-full">
+          <div className="bg-gray-400 animate-pulse rounded-md w-[50%] h-full" />
+          <div className="bg-gray-400 animate-pulse rounded-md w-[20%] h-full" />
+        </div>
+        <div className="bg-gray-400 animate-pulse rounded-full size-[30px]" />
+      </div>
+      <div className="border-b py-3 px-5 h-[67px] flex justify-between items-center">
+        <div className="flex flex-col gap-1 grow h-full">
+          <div className="bg-gray-400 animate-pulse rounded-md w-[50%] h-full" />
+          <div className="bg-gray-400 animate-pulse rounded-md w-[20%] h-full" />
+        </div>
+        <div className="bg-gray-400 animate-pulse rounded-full size-[30px]" />
+      </div>
+      <div className="border-b py-3 px-5 h-[67px] flex justify-between items-center">
+        <div className="flex flex-col gap-1 grow h-full">
+          <div className="bg-gray-400 animate-pulse rounded-md w-[50%] h-full" />
+          <div className="bg-gray-400 animate-pulse rounded-md w-[20%] h-full" />
+        </div>
+        <div className="bg-gray-400 animate-pulse rounded-full size-[30px]" />
+      </div>
+    </div>
+  );
 };
 
 export default PostsLoading;
